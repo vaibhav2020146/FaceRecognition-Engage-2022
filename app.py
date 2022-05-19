e9c9e7fd3267533dbf2a4fe9db4ea6d5483e35a5
creator-26 <vaibhav20146@iiitd.ac.in>
2022-05-19 14:26:15 +0530

    New Features

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask,render_template,redirect,request
+import openpyxl
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 import cv2
 import numpy as np
 import face_recognition
 import os
-from datetime import datetime
+from os.path import exists
+from datetime import datetime,date
+import pandas as pd
+from openpyxl import load_workbook
+import csv
 
 app=Flask(__name__)
-database={'vaibhav':['1234','vaibhavrajpal26@gmail.com'],'shubham':['5678','ab@yahoo.com'],'shivam':['abcd','xyz@gmail.com'],'elon':['tesla','spacex@gmail.com']}
+database={'vaibhav':['1234','vaibhavrajpal26@gmail.com'],'vrinda':['5678','ab@yahoo.com'],'shivam':['abcd','xyz@gmail.com'],'elon':['tesla','spacex@gmail.com']}
 #will make database like key would be the username and value would be list of password and mail
+
+@app.route('/table')
+def table():
+    df = pd.read_excel('Attendance.xlsx')
+    df.to_excel('Attendance.xlsx', index=None)
+    data = pd.read_excel('Attendance.xlsx')
+    return render_template('/attendance_record.html', tables=[data.to_html()], titles=[''])
+
+
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLD = 'static/uploads'
@@ -49,7 +63,7 @@ def form_login():
                 # # make the attendence webpage and change the below location to it
                 return render_template('/login_page.html',info='Invalid Password')
             else:
-                return render_template('/index.html',info='All Correct')
+                return render_template('/mark_attendance.html')
     return render_template('/login_page.html')
 
 
@@ -70,7 +84,7 @@ def home():
 
 @app.route("/about", methods=['GET', 'POST'])
 def about():
-    return render_template("about.html")               
+    return render_template("/about.html")               
 
 @app.route("/login")
 def login():
@@ -98,17 +112,47 @@ def login():
     
     def markAttendance(name):
         #will mark the attendance of the student and save it in the csv file
-        with open('C:\\Users\\91991\\Desktop\\Project\\Attendance.csv', 'r+') as f:
+        #df = pd.read_excel("Attendance.xlsx", usecols = ['Username'])
+        #print(df)
+        with open('Attendance.csv', 'r+') as f:
             myDataList = f.readlines()
             nameList = []
+            print(nameList)
+            if(name in myDataList):
+                return True
+
             for line in myDataList:
                 entry = line.split(',')
                 nameList.append(entry[0])
             if name not in nameList:
                 now = datetime.now()
+                date_today=date.today();
                 dtString = now.strftime('%H:%M:%S')
-                f.writelines(f'n{name},{dtString}')
+                convert_to_format=date_today.strftime("%d-%m-%Y")
+                #f.writelines(f'{name},{dtString},{convert_to_format}')
+                '''writer = pd.ExcelWriter('Attendance.xlsx', engine='openpyxl') 
+                wb  = writer.book
+                df = pd.DataFrame({'Username': [name],
+                    'Date': [convert_to_format],
+                  'Time': [dtString]})
+                df.to_excel(writer, index=False)
+                wb.save('Attendance.xlsx')'''
+                wb=openpyxl.load_workbook('Attendance.xlsx')
+                sh1=wb['Sheet1']
+                row=sh1.max_row
+                column=sh1.max_column
+                is_added=False
+                for i in range(1,row+1):
+                    if (sh1.cell(row=i,column=1).value==name) and (sh1.cell(row=i,column=3).value==convert_to_format):
+                            is_added=True
 
+                if(is_added==False):
+                    sh1.cell(row=row+1,column=1,value=name)
+                    sh1.cell(row=row+1,column=2,value=dtString)
+                    sh1.cell(row=row+1,column=3,value=convert_to_format)
+                wb.save('Attendance.xlsx')
+                return True
+        return False
     encodeListKnown = findEncodings(images)
     print('Encoding Complete')
     #open the camera and takes the attendence
@@ -124,7 +168,7 @@ def login():
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
+        to_close_the_web_cam=False
         for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
             matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
             faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
@@ -139,15 +183,20 @@ def login():
                 cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
                 cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-                markAttendance(name)
+                to_close_the_web_cam=markAttendance(name)
+            if(to_close_the_web_cam):
+                break
         cv2.imshow('Webcam', img)
         cv2.waitKey(1)
+        if(to_close_the_web_cam):
+            break
     cap.release()
     cv2.destroyAllWindows()
     #return render_template("index.html")
-    return redirect("/")
+    return render_template("/mark_attendance.html",info='Your Attendance Has Been Taken')
 
 
 if __name__=="__main__":
     #database.clear()
+
     app.run(debug=True)
